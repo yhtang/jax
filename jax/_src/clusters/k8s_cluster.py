5506af68e09eb7fdcd86268d8f1d4b7a754e7794
Yu-Hang Tang <Tang.Maxin@gmail.com>
2024-07-08 05:53:08 +0000

    cleanup

--- jax/_src/clusters/k8s_cluster.py
+++ jax/_src/clusters/k8s_cluster.py
@@ -18,7 +18,10 @@ class K8sCluster(clusters.ClusterEnv):
 
   @classmethod
   def is_env_present(cls) -> bool:
-    if 'KUBERNETES_SERVICE_HOST' in os.environ:
+    if all([
+      'KUBERNETES_SERVICE_HOST' in os.environ,
+      'POD_NAME' in os.environ
+    ]):
       try:
         import kubernetes as k8s  # pytype: disable=import-error
       except ImportError as e:
@@ -33,33 +36,31 @@ class K8sCluster(clusters.ClusterEnv):
         print('--------------------------------------------------------')
         raise e
       k8s.config.load_incluster_config()
-      cls.core_api = k8s.client.CoreV1Api()
-      cls.batch_api = k8s.client.BatchV1Api()
-      cls.ApiException = k8s.client.exceptions.ApiException
+      cls._core_api = k8s.client.CoreV1Api()
+      cls._batch_api = k8s.client.BatchV1Api()
+      cls._ApiException = k8s.client.exceptions.ApiException
       return True
     else:
       return False
 
   @classmethod
   @cache
-  def pod_name(cls):
+  def _pod_name(cls):
     return os.getenv('POD_NAME')
-    # if pod_name is None:
-    #   raise ValueError('Cannot find environment variable: POD_NAME')
 
   @classmethod
   @cache
-  def namespace(cls):
+  def _namespace(cls):
     return open(
       '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
     ).read().strip()
 
   @classmethod
   @contextmanager
-  def handle_api_exception(cls):
+  def _handle_api_exception(cls):
     try:
       yield
-    except cls.ApiException as e:
+    except cls._ApiException as e:
       print(f"Kubernetes API Error: {e.status} - {e.reason}")
       if e.status == 403:
         print('--------------------------------------------------------')
@@ -76,40 +77,39 @@ class K8sCluster(clusters.ClusterEnv):
 
   @classmethod
   @cache
-  def pod(cls):
-    with cls.handle_api_exception():
-      return cls.core_api.read_namespaced_pod(
-        name=cls.pod_name(), namespace=cls.namespace()
+  def _pod(cls):
+    with cls._handle_api_exception():
+      return cls._core_api.read_namespaced_pod(
+        name=cls._pod_name(), namespace=cls._namespace()
       )
 
   @classmethod
   @cache
-  def job(cls):
-    with cls.handle_api_exception():
-      return cls.batch_api.read_namespaced_job(
-        name=cls.pod().metadata.labels['job-name'], namespace=cls.namespace()
+  def _job(cls):
+    with cls._handle_api_exception():
+      return cls._batch_api.read_namespaced_job(
+        name=cls._pod().metadata.labels['job-name'], namespace=cls._namespace()
       )
 
   @classmethod
   def get_coordinator_address(cls, timeout_secs: int | None) -> str:
     return '{job_name}-0.{jobset_name}:{port}'.format(
-      job_name=cls.pod().metadata.labels['job-name'],
-      jobset_name=cls.job().metadata.labels['jobset.sigs.k8s.io/jobset-name'],
+      job_name=cls._pod().metadata.labels['job-name'],
+      jobset_name=cls._job().metadata.labels['jobset.sigs.k8s.io/jobset-name'],
       port=cls._coordinator_port
     )
 
   @classmethod
   def get_process_count(cls) -> int:
-    # Reference: https://kubernetes.io/docs/concepts/workloads/controllers/job/#controlling-parallelism
-    # pods/<POD_NAME> | .metadata.labels.job-name
-    # jobs/<JOB_NAME> | .metadata.spec.parallelism
-    return cls.job().spec.parallelism
+    # https://kubernetes.io/docs/concepts/workloads/controllers/job/#controlling-parallelism
+    return cls._job().spec.parallelism
 
   @classmethod
   def get_process_id(cls) -> int:
-    # Reference: https://kubernetes.io/docs/concepts/workloads/controllers/job/#completion-mode
-    # JOB_COMPETION_INDEX
+    # https://kubernetes.io/docs/concepts/workloads/controllers/job/#completion-mode
     try:
       return int(os.environ['JOB_COMPLETION_INDEX'])
     except KeyError:
-      raise RuntimeError('K8s job must be run with `completionMode: "Indexed"`.')
+      raise RuntimeError(
+        'K8s job must be run with `completionMode: "Indexed"`.'
+      )
